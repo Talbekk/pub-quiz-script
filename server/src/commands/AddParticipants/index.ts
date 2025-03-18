33d994334f948f13cf869a0150e088c394600c44
Michael Campbell <mt.campbell@outlook.com>
2025-03-18 17:50:15 +0000

    adds AddParticipants script command

---
+++ server/src/commands/AddParticipants/index.ts
@@ -0,0 +1,30 @@
+import { ulid } from "ulid";
+import { connectDB, getDB } from "../../config/db";
+import groupChat from "../../data/group-chat.json";
+
+type Participant = {
+    _id: string;
+    full_name: string;
+}
+
+export const AddParticipants = async () => {
+    const db = getDB();
+    const participantsCollection = db.collection<Participant>("participants");
+    const participants = groupChat.participants;
+    console.log(`formattedData: `, participants);
+    const formattedParticipants = participants.map((participant) => {
+        return {
+            _id: ulid(),
+            full_name: participant.name,
+        }
+    })
+    try {
+        const result = await participantsCollection.insertMany(formattedParticipants, { ordered: true });
+        console.log(`${result.insertedCount} documents were inserted`);
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+connectDB();
+AddParticipants();
