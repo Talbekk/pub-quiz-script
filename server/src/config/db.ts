37caae9f04c959ec4708ad3f1d10a41d41de39f0
Michael Campbell <mt.campbell@outlook.com>
2025-03-18 17:49:23 +0000

    adds basic server and mongodb client

---
+++ server/src/config/db.ts
@@ -0,0 +1,28 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI as string;
+
+let client: MongoClient;
+
+const connectDB = async () => {
+  try {
+    client = new MongoClient(MONGO_URI);
+    await client.connect();
+    console.log("✅ MongoDB connected successfully!");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
+
+const getDB = () => {
+  if (!client) {
+    throw new Error("MongoDB client is not initialized.");
+  }
+  return client.db(process.env.DB_NAME);
+};
+
+export { connectDB, getDB };
