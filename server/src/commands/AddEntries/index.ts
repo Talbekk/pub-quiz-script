f6ceec8ae93e24f5a88e50cbd9aa341f6b96ee40
Michael Campbell <mt.campbell@outlook.com>
2025-03-20 18:59:57 +0000

    adds slightly broken script, too many entries

--- server/src/commands/AddEntries/index.ts
+++ server/src/commands/AddEntries/index.ts
@@ -1,4 +1,4 @@
-import { connectDB, getDB, close  } from "../../config/db";
+import { connectDB, getDB, close } from "../../config/db";
 import { Message } from "../AddMessages";
 import { ulid } from "ulid";
 import { Quiz } from "../AddQuizzes";
@@ -8,16 +8,18 @@ import batch from "../Services/BatchStream";
 export type Entry = {
     _id: string;
     player_ref: string;
+    quiz_ref: string;
     score: number | null;
     messages: Array<Message>;
     created_at: number;
 }
 
-const createEntry = (participantId: Participant['_id'], messages: Array<Message>): Entry => {
+const createEntry = (participantId: Participant['_id'], quizId: Quiz['_id'], messages: Array<Message>): Entry => {
     const participantMessages: Array<Message> = messages.filter(message => message.participant_ref === participantId);
     return {
         _id: ulid(),
         player_ref: participantId,
+        quiz_ref: quizId,
         score: null,
         messages: participantMessages,
         created_at: Date.now(),
@@ -39,19 +41,16 @@ const AddEntries = async () => {
         const batchSize = 25;
 
         const processQuizBatch = async (quizzes: Array<Quiz>, messages: Array<Message>) => {
-            const quizIds: Array<Quiz['_id']> = quizzes.map(quiz => quiz._id);
             let entries: Array<Entry> = [];
             const updatedQuizzes: Array<Quiz> = quizzes.map((quiz) => {
                 const messagesDuringQuizWeek = messages.filter(message => message.timestamp >= quiz.start_datetime && message.timestamp < quiz.end_datetime);
-                const quizEntries = participants.map((participant) => createEntry(participant._id, messagesDuringQuizWeek));
+                const quizEntries = participants.map((participant) => createEntry(participant._id, quiz._id, messagesDuringQuizWeek));
                 entries.push(...quizEntries);
                 return {
                     ...quiz,
                     entries: quizEntries,
                 }
             });
-            console.log(`updatedQuizzes: `, updatedQuizzes);
-            console.log(`entries: `, entries);
             const bulkUpdate = updatedQuizzes.map((updatedQuiz) => {
                 return {
                     updateOne: {
