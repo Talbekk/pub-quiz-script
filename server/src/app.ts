3225570bba7f81965c33821e5de951cf0536d94e
Michael Campbell <mt.campbell@outlook.com>
2025-04-29 17:16:16 +0100

    adds basic setup for API

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,16 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db';
+import itemRoutes from './routes/itemRoutes';
+import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 
+app.use('/items', itemRoutes);
+app.use(errorHandler);
 app.use(cors());
 app.use(express.json());
 
@@ -14,6 +18,4 @@ app.get('/', async (req: Request, res: Response) => {
     res.send('Hello, TypeScript with Express!');
 });
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
+export default app;
