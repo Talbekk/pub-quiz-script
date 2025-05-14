3f71b60249f6572cf65369fdec2e63071f63081b
Michael Campbell <mt.campbell@outlook.com>
2025-05-14 17:07:53 +0100

    adds mocks

---
+++ server/src/test_utils/__mocks__/entries/index.ts
@@ -0,0 +1,4 @@
+export const mockEntry1 = { id: '1', created_at: 1234567890, messages: [], player_ref: 'player1', possible_scores: [], quiz_ref: 'quiz1', score: null, validated: false };
+export const mockEntry2 = { id: '2', created_at: 1234567891, messages: [], player_ref: 'player2', possible_scores: [], quiz_ref: 'quiz2', score: null, validated: false };
+
+export const mockEntries = [mockEntry1, mockEntry2];
---
+++ server/src/test_utils/__mocks__/pagination/index.ts
@@ -0,0 +1,6 @@
+export const requestPagination = {
+    skip: 0,
+    take: 10,
+    page: 1,
+    limit: 10,
+};
