7cb0b3d1f8f5728743522bc7f2bf379d20a5a364
Michael Campbell <mt.campbell@outlook.com>
2025-05-15 11:22:20 +0100

    adds mock quiz

---
+++ server/src/test_utils/__mocks__/quizzes/index.ts
@@ -0,0 +1,22 @@
+export const mockQuiz1 = {
+    id: '1',
+    created_at: 1234567890,
+    start_datetime: 1234567890,
+    end_datetime: 1234567891,
+    entries: [],
+    url: []
+};
+
+export const mockQuiz2 = {
+    id: '2',
+    created_at: 1234567890,
+    start_datetime: 1234567890,
+    end_datetime: 1234567891,
+    entries: [],
+    url: []
+};
+
+export const mockQuizzes = [
+    mockQuiz1,
+    mockQuiz2,
+];
