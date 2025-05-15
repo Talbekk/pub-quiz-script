de8da6f26672fde698b4f6167c0b91095179cdcb
Michael Campbell <mt.campbell@outlook.com>
2025-05-15 11:22:09 +0100

    adds mock participants

---
+++ server/src/test_utils/__mocks__/participants/index.ts
@@ -0,0 +1,6 @@
+export const mockParticipant1 = { id: '1', full_name: 'Bruce Wayne' };
+export const mockParticipant2 = { id: '2', full_name: 'James Bond' };
+export const mockParticipants = [
+    mockParticipant1,
+    mockParticipant2,
+];
