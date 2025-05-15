575ea5ee0abc801c15ed9125a0420c133af95a91
Michael Campbell <mt.campbell@outlook.com>
2025-05-15 11:22:34 +0100

    adds quiz service

---
+++ server/src/services/quizService/index.ts
@@ -0,0 +1,18 @@
+import prisma from "../../client";
+
+export const getAllQuizzes = async () => {
+    const quizzes = await prisma.quizzes.findMany();
+    return quizzes;
+};
+
+export const getQuizByID = async (quizid: string) => {
+    const quiz = await prisma.quizzes.findFirst({
+        where: {
+            id: quizid,
+        },
+    });
+    if (!quiz) {
+        throw new Error('Quiz not found');
+    }
+    return quiz;
+};
