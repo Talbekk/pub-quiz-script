2198a831dd3dabf75e935491438a84a6cdf1d6fb
Michael Campbell <mt.campbell@outlook.com>
2025-05-16 12:30:54 +0100

    adds ThrowError class

--- server/src/middlewares/errorHandler/index.ts
+++ server/src/middlewares/errorHandler/index.ts
@@ -1,17 +1,34 @@
 import { Request, Response, NextFunction } from 'express';
 
-export interface AppError extends Error {
-    status?: number;
+export class ThrowError extends Error {
+    statusCode: number;
+    data: { [key: string]: any };
+
+    constructor(statusCode: number, message: string, data: { [key: string]: any } = {}) {
+        super(message);
+        this.statusCode = statusCode;
+        this.data = data;
+    }
 }
 
 export const errorHandler = (
-    err: AppError,
+    err: Error | ThrowError,
     req: Request,
     res: Response,
     next: NextFunction,
 ) => {
     console.error(err);
-    res.status(err.status || 500).json({
-        message: err.message || 'Internal Server Error',
+    if (err instanceof ThrowError) {
+        res.status(err.statusCode).json({
+            status: false,
+            message: err.message,
+            data: err.data,
+        });
+    }
+    // Handle unexpected errors
+    res.status(500).json({
+        status: false,
+        message: err?.message || 'Internal Server Error',
+        data: {},
     });
 };
