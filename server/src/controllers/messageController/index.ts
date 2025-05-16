a6258d5dc0fc40b406ccf48a2b6422d8510b5349
Michael Campbell <mt.campbell@outlook.com>
2025-05-16 12:43:30 +0100

    adds try catch blocks to message controller

--- server/src/controllers/messageController/index.ts
+++ server/src/controllers/messageController/index.ts
@@ -1,25 +1,33 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { generatePaginatedResponse } from '../../services/generatePaginatedResponse';
 import { getMessageByID, getPaginatedMessages } from '../../services/messageService';
 
-export const getMessages = async (req: Request, res: Response) => {
-    const { messageCount, messages } = await getPaginatedMessages(req.pagination!);
-    const response = generatePaginatedResponse({
-        status: true,
-        message: 'Messages Successfully fetched',
-        data: messages,
-        collectionCount: messageCount,
-        requestPagination: req.pagination!,
-    });
-    res.json(response);
+export const getMessages = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { messageCount, messages } = await getPaginatedMessages(req.pagination!);
+        const response = generatePaginatedResponse({
+            status: true,
+            message: 'Messages Successfully fetched',
+            data: messages,
+            collectionCount: messageCount,
+            requestPagination: req.pagination!,
+        });
+        res.json(response);
+    } catch (error) {
+        next(error);
+    }
 };
 
-export const getMessage = async (req: Request, res: Response) => {
+export const getMessage = async (req: Request, res: Response, next: NextFunction) => {
     const { messageid } = req.params;
-   const message = await getMessageByID(messageid);
-    res.json({
-        status: true,
-        message: 'Message Successfully fetched',
-        data: message,
-    });
+    try {
+        const message = await getMessageByID(messageid);
+        res.json({
+            status: true,
+            message: 'Message Successfully fetched',
+            data: message,
+        });
+    } catch (error) {
+        next(error);
+    }
 };
