0555d9774adaf71beef33bcadfb6452b5987f872
Michael Campbell <mt.campbell@outlook.com>
2025-05-19 16:55:35 +0100

    initialise and add passport to app

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,23 @@ import express from 'express';
 import cors from 'cors';
 import indexRoute from './routes';
 import { errorHandler } from './middlewares/errorHandler';
+import session from "express-session";
+import passport from 'passport';
+import './config/passport';
 
 const app = express();
+
+app.use(session({
+    secret: process.env.SESSION_TOKEN!,
+    resave: false,
+    saveUninitialized: true,
+}));
 
 app.use(cors());
 app.use(express.json());
 app.use('/', indexRoute);
 app.use(errorHandler);
+app.use(passport.initialize());
+app.use(passport.session());
 
 export default app;
