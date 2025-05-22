966b3ffaa2b6161e308b2349c80b9a28c16071d5
Michael Campbell <mt.campbell@outlook.com>
2025-05-22 16:51:38 +0100

    adds tanstack router

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+    react()
+],
   test: {
     globals: true,
     environment: 'jsdom',
