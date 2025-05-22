95417780dc664eab64099c367436ac66c2aa2794
Michael Campbell <mt.campbell@outlook.com>
2025-05-22 16:22:58 +0100

    initial setup for front end

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+
+export const App = () => {
+    return (
+        <>
+            <h1>Pub Quiz</h1>
+            <p>Test your knowledge with our pub quiz!</p>
+        </>
+    );
+};
