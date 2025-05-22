ad5f432e77ddd348f9e7a3064fad399ee2038e2c
Michael Campbell <mt.campbell@outlook.com>
2025-05-22 16:33:23 +0100

    adds header component

---
+++ client/src/components/Header/index.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import { Header } from '.';
+
+describe("Header", () => {
+    it("renders correctly", () => {
+        render(<Header />);
+        expect(screen.getByText("BR-DGE - Tech Test Interview")).toBeInTheDocument();
+    });
+});
---
+++ client/src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <header>
+              <h1>BR-DGE - Tech Test Interview</h1>
+        </header>
+    );
+};
