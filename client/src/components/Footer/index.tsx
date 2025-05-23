deec7312d6fcacb26a61a35689377a0064ced396
Michael Campbell <mt.campbell@outlook.com>
2025-05-23 11:57:33 +0100

    cleans up unused components

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -1,11 +1,14 @@
-import type { FunctionComponent } from "react";
+import type { FunctionComponent } from 'react';
 
 export const Footer: FunctionComponent = () => {
     return (
         <footer>
             <div>
-                <p>&copy; {new Date().getFullYear()} CodingNomad. All rights reserved.</p>
+                <p>
+                    &copy; {new Date().getFullYear()} CodingNomad. All rights
+                    reserved.
+                </p>
             </div>
         </footer>
-    )
+    );
 };
