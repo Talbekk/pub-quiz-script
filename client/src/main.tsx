9a669f05da3f38b0c461c2acdda61397e17c15db
Michael Campbell <mt.campbell@outlook.com>
2025-05-26 21:11:21 +0100

    wrapps app in query client provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,43 +1,48 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
 import { routeTree } from './routeTree.gen';
 import { AuthProvider, useAuth, type AuthContextType } from './services/auth';
 
+const queryClient = new QueryClient();
+
 const router = createRouter({
-  routeTree,
-  defaultPreload: 'intent',
-  scrollRestoration: true,
-  context: {
-    auth: {} as AuthContextType,
-  },
+    routeTree,
+    defaultPreload: 'intent',
+    scrollRestoration: true,
+    context: {
+        auth: {} as AuthContextType,
+    },
 });
 declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router;
-  }
+    interface Register {
+        router: typeof router;
+    }
 }
 
 const InnterApp = () => {
-  const auth = useAuth();
-  return <RouterProvider router={router} context={{ auth }} />;
+    const auth = useAuth();
+    return <RouterProvider router={router} context={{ auth }} />;
 };
 
 const App = () => {
-  return (
-    <AuthProvider>
-      <InnterApp />
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <InnterApp />
+        </AuthProvider>
+    );
 };
 
 const rootElement = document.getElementById('root')!;
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+        <StrictMode>
+            <QueryClientProvider client={queryClient}>
+                <App />
+            </QueryClientProvider>
+        </StrictMode>,
+    );
 }
