75a78671598fc2e2a5c659a6d0825b7da7d1b7ee
Michael Campbell <mt.campbell@outlook.com>
2025-05-26 21:11:53 +0100

    adds client url to cors allowance for server

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,7 +15,12 @@ app.use(
         saveUninitialized: true,
     }),
 );
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    }),
+);
 app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
