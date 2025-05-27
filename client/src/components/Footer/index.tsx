db33ef394c448dccf2785ad0a8e40de32ec6e1d3
Michael Campbell <mt.campbell@outlook.com>
2025-05-27 18:30:10 +0100

    updates footer

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -1,14 +1,13 @@
 import type { FunctionComponent } from 'react';
+import styles from './styles.module.scss';
 
 export const Footer: FunctionComponent = () => {
     return (
-        <footer>
-            <div>
-                <p>
-                    &copy; {new Date().getFullYear()} CodingNomad. All rights
-                    reserved.
-                </p>
-            </div>
+        <footer className={styles.footer}>
+            <p>
+                &copy; {new Date().getFullYear()} CodingNomad. All rights
+                reserved.
+            </p>
         </footer>
     );
 };
