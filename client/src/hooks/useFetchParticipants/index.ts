7c49135d80f35ba2f46573cd6aeaf7956e3ee807
Michael Campbell <mt.campbell@outlook.com>
2025-05-27 17:02:59 +0100

    adds useFetchParticipants endpoint

---
+++ client/src/hooks/useFetchParticipants/index.ts
@@ -0,0 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
+import type { QueryObserverResult } from '@tanstack/react-query';
+import type { AxiosResponse } from 'axios';
+import { quizAPI } from '../../api/client';
+
+interface FetchParticipantsResponse {
+    data: any[];
+    message: string;
+    status: boolean;
+}
+
+const fetchParticipants = async (): Promise<
+    AxiosResponse<FetchParticipantsResponse>
+> => {
+    return await quizAPI.get<FetchParticipantsResponse>('/participants/');
+};
+
+export const useFetchParticipants = (): QueryObserverResult<
+    FetchParticipantsResponse,
+    any
+> => {
+    return useQuery<FetchParticipantsResponse, any>({
+        queryFn: async () => {
+            const { data } = await fetchParticipants();
+            return data;
+        },
+        queryKey: ['participants'],
+    });
+};
