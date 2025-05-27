292bb9d8a97697409e07933a1e52a5af63eea655
Michael Campbell <mt.campbell@outlook.com>
2025-05-27 17:52:25 +0100

    moves auth into folder

--- client/src/hooks/useAuth/useAuthMutations.ts
+++ client/src/hooks/useAuth/useAuthMutations.ts
@@ -1,6 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { authApi } from '../api/auth';
-import { useAuth } from '../services/auth';
+import { authApi } from '../../api/auth';
+import { useAuth } from '../../services/auth';
 
 export const useLogin = () => {
     const { setUser } = useAuth();
